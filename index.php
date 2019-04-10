c0201f9d732baacd99640bbef8548d6599d2ddb4
Muhammad Iqbal <chongg1243@gmail.com>
2019-04-10 19:06:42 +0700

    change config db

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "chongieballserver.database.windows.net";
+    $user = "chongieball";
+    $pass = "azureazure123@";
+    $db = "chongieball";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
